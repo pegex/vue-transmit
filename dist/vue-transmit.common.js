897ce2a45652935b2c4e23ff4e85b14fdcbea44f
James McKenzie <james.mckenzie@pegex.com>
2018-09-20 15:51:32 -0500

    Update dist

--- dist/vue-transmit.common.js
+++ dist/vue-transmit.common.js
@@ -1170,7 +1170,7 @@ __decorate([(0, _vuePropertyDecorator.Prop)({ type: [Array, Object, String], def
 __decorate([(0, _vuePropertyDecorator.Prop)({ type: Object, default: _utils.objFactory })], VueTransmit.prototype, "uploadAreaAttrs", void 0);
 __decorate([(0, _vuePropertyDecorator.Prop)({ type: Object, default: _utils.objFactory })], VueTransmit.prototype, "uploadAreaListeners", void 0);
 __decorate([(0, _vuePropertyDecorator.Prop)({ type: String, default: null })], VueTransmit.prototype, "dragClass", void 0);
-__decorate([(0, _vuePropertyDecorator.Prop)({ type: String, required: true })], VueTransmit.prototype, "url", void 0);
+__decorate([(0, _vuePropertyDecorator.Prop)({ type: String, required: false })], VueTransmit.prototype, "url", void 0);
 __decorate([(0, _vuePropertyDecorator.Prop)({ type: String, default: "post" })], VueTransmit.prototype, "method", void 0);
 __decorate([(0, _vuePropertyDecorator.Prop)({ type: Boolean, default: false })], VueTransmit.prototype, "withCredentials", void 0);
 __decorate([(0, _vuePropertyDecorator.Prop)({ type: Number, default: 0 })], VueTransmit.prototype, "timeout", void 0);
@@ -1393,7 +1393,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_ts_loader_node_modules_vue_loader_lib_selector_type_script_index_0_VueTransmit_vue__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_ts_loader_node_modules_vue_loader_lib_selector_type_script_index_0_VueTransmit_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_ts_loader_node_modules_vue_loader_lib_selector_type_script_index_0_VueTransmit_vue__);
 /* harmony namespace reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_ts_loader_node_modules_vue_loader_lib_selector_type_script_index_0_VueTransmit_vue__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_ts_loader_node_modules_vue_loader_lib_selector_type_script_index_0_VueTransmit_vue__[key]; }) }(__WEBPACK_IMPORT_KEY__));
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_34e59ce8_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_VueTransmit_vue__ = __webpack_require__(16);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_ed1e5922_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_VueTransmit_vue__ = __webpack_require__(16);
 function injectStyle (ssrContext) {
   __webpack_require__(6)
 }
@@ -1413,7 +1413,7 @@ var __vue_scopeId__ = null
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
   __WEBPACK_IMPORTED_MODULE_0__babel_loader_vue_ts_loader_node_modules_vue_loader_lib_selector_type_script_index_0_VueTransmit_vue___default.a,
-  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_34e59ce8_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_VueTransmit_vue__["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_template_compiler_index_id_data_v_ed1e5922_hasScoped_false_buble_transforms_node_modules_vue_loader_lib_selector_type_template_index_0_VueTransmit_vue__["a" /* default */],
   __vue_template_functional__,
   __vue_styles__,
   __vue_scopeId__,
